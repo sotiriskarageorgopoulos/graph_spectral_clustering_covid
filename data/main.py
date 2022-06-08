046b185eaadca5ce4a9ecb412a5e37aa3493e933
sotiriskarageorgopoulos <sotiriskarageorgopoulos@gmail.com>
2022-06-08 13:39:58 +0300

    graph clustering for all countries is ok!

--- data/main.py
+++ data/main.py
@@ -1,17 +1,19 @@
 import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
 # read csv to a dataframe
 df = pd.read_csv('covid_19_test_cases.csv')
 df_vac = pd.read_csv('eu_vaccinations.csv')
 df_vac.rename(columns={'ReportingCountry': 'geoId'}, inplace=True)
 
-# sum calculation of cases and deaths for 2020 per country
+# sum calculation of cases and deaths for 2020 per eu country
 df_2020 = df[df['year'] < 2021].groupby("geoId").agg(
     {'cases': sum, 'deaths': sum})
 
 df_2020["IFR"] = df_2020["deaths"].div(df_2020["cases"].values)
 df_2020.to_csv('eu_covid_19(2020).csv')
 
-# sum calculation of cases and deaths for 2021-2022 per country
+# sum calculation of cases and deaths for 2021-2022 per eu country
 df_2021_2022 = df[df['year'] >= 2021].groupby("geoId").agg(
     {'cases': sum, 'deaths': sum})
 df_2021_2022["IFR"] = df_2021_2022["deaths"].div(df_2021_2022["cases"].values)
@@ -22,3 +24,83 @@ covid_19_all = pd.merge(df_vac_21_22, df_2021_2022,
                  how='inner')
 
 covid_19_all.to_csv('eu_covid_19(2021-2022).csv')
+
+def create_csv_by_continent(world_data:pd.DataFrame,continent:str,period:str):
+    cond_cases_vacc = ""
+    cond_deaths_vacc = ""
+    if period == "after":
+        cond_cases_vacc = (~world_data["country"].str.contains("(total)")) & (world_data["indicator"] == 'cases') & (world_data["year_week"].str.contains("2021") | world_data["year_week"].str.contains("2022")) & (world_data["continent"] == continent)
+        cond_deaths_vacc  = (~world_data["country"].str.contains("(total)")) & (world_data["indicator"] == 'deaths') & (world_data["year_week"].str.contains("2021") | world_data["year_week"].str.contains("2022")) & (world_data["continent"] == continent)
+    elif period == "before":
+        cond_cases_vacc = (~world_data["country"].str.contains("(total)")) & (world_data["indicator"] == 'cases') & (world_data["year_week"].str.contains("2020")) & (world_data["continent"] == continent)
+        cond_deaths_vacc = (~world_data["country"].str.contains("(total)")) & (world_data["indicator"] == 'deaths') & (world_data["year_week"].str.contains("2020")) & (world_data["continent"] == continent)
+    else:
+        print("Wrong period...")
+   
+    cases_vacc = world_data[cond_cases_vacc].groupby('country_code')["weekly_count"].sum()
+    deaths_vacc = world_data[cond_deaths_vacc].groupby('country_code')["weekly_count"].sum()
+    new_df = pd.merge(cases_vacc,deaths_vacc,on="country_code",how="inner")
+    new_df.rename(columns={'weekly_count_x': 'cases'}, inplace=True)
+    new_df.rename(columns={'weekly_count_y': 'deaths'}, inplace=True)
+    new_df["IFR"] = new_df["deaths"].div(new_df["cases"].values)
+    
+    if period == "before":
+        new_df.to_csv(f"{continent.lower()}_continent_covid(2020).csv")
+    elif period == "after":
+        new_df.to_csv(f"{continent.lower()}_continent_covid(2021-2022).csv")
+
+world_data = pd.read_csv("../data/worldwide_data.csv")
+
+#Europe continent stats before vaccinations
+create_csv_by_continent(world_data,"Europe","before")
+#Europe continent stats after vaccinations
+create_csv_by_continent(world_data,"Europe","after")
+
+#Asia continent stats before vaccinations
+create_csv_by_continent(world_data,"Asia","before")
+#Asia continent stats after vaccinations
+create_csv_by_continent(world_data,"Asia","after")
+
+#Oceania continent stats before vaccinations
+create_csv_by_continent(world_data,"Oceania","before")
+#Oceania continent stats after vaccinations
+create_csv_by_continent(world_data,"Oceania","after")
+
+#America continent stats before vaccinations
+create_csv_by_continent(world_data,"America","before")
+#America continent stats after vaccinations
+create_csv_by_continent(world_data,"America","after")
+
+def create_continents_csv(world_data,period):
+    cond_cases_continents = ""
+    cond_deaths_continents = ""
+    if period == 'before':
+        cond_cases_continents = (world_data["country"].str.contains("(total)")) & (world_data["indicator"] == 'cases') & (~world_data["country"].str.contains("EU/EEA")) & (world_data["year_week"].str.contains("2020"))
+        cond_deaths_continents = (world_data["country"].str.contains("(total)")) & (world_data["indicator"] == 'deaths') & (~world_data["country"].str.contains("EU/EEA")) & (world_data["year_week"].str.contains("2020"))
+    elif period == 'after':
+        cond_cases_continents = (world_data["country"].str.contains("(total)")) & (world_data["indicator"] == 'cases') & (~world_data["country"].str.contains("EU/EEA")) & (world_data["year_week"].str.contains("2021") | world_data["year_week"].str.contains("2022"))
+        cond_deaths_continents = (world_data["country"].str.contains("(total)")) & (world_data["indicator"] == 'deaths') & (~world_data["country"].str.contains("EU/EEA")) & (world_data["year_week"].str.contains("2021") | world_data["year_week"].str.contains("2022"))
+    
+    continents_cases = world_data[cond_cases_continents].groupby("country")["weekly_count"].sum()
+    continents_deaths = world_data[cond_deaths_continents].groupby("country")["weekly_count"].sum()
+    new_df = pd.merge(continents_cases,continents_deaths,on="country",how="inner")
+    new_df.rename(columns={'weekly_count_x': 'cases'}, inplace=True)
+    new_df.rename(columns={'weekly_count_y': 'deaths'}, inplace=True)
+    new_df["IFR"] = new_df["deaths"].div(new_df["cases"].values)
+    continents = {
+       "code": ['AF','AM','AS','EU','OC'],
+       "cases": new_df["cases"].to_list(),
+       "deaths": new_df["deaths"].to_list(),
+       "IFR": new_df["IFR"].to_list()
+    }
+    continents_df = pd.DataFrame.from_dict(continents)
+    
+    if period == 'before':
+        continents_df.to_csv('continents_covid(2020).csv',index=False)
+    elif period == 'after':
+        continents_df.to_csv('continents_covid(2021-2022).csv',index=False)
+     
+#Continents stats before vaccinations   
+create_continents_csv(world_data,"before")
+#Continents stats after vaccinations 
+create_continents_csv(world_data,"after")
--- main.py
+++ main.py
@@ -1,19 +1,83 @@
 import pandas as pd
 from do_graph_clustering import do_graph_clustering 
 
-#Cluster of countries according to cases and deaths before vaccinations
+#Cluster of countries according to cases, deaths and IFR before vaccinations in EU
 
-covid_stats_bef_vacc = pd.read_csv("./data/eu_covid_19(2020).csv")
-covid_stats_bef_vacc.fillna(method="ffill",inplace=True)
-norm_df_bef_vacc = (covid_stats_bef_vacc[['cases','deaths']]-covid_stats_bef_vacc[['cases','deaths']].mean())/(covid_stats_bef_vacc[['cases','deaths']].max()-covid_stats_bef_vacc[['cases','deaths']].min())
-covid_stats_bef_vacc.loc[:,['cases','deaths']] = norm_df_bef_vacc[['cases','deaths']]
-do_graph_clustering(covid_stats_bef_vacc,"Cluster of countries before vaccination")
+covid_stats_bef_eu = pd.read_csv("./data/eu_covid_19(2020).csv")
+norm_df_bef_eu = (covid_stats_bef_eu[['cases','deaths']]-covid_stats_bef_eu[['cases','deaths']].mean())/(covid_stats_bef_eu[['cases','deaths']].max()-covid_stats_bef_eu[['cases','deaths']].min())
+covid_stats_bef_eu.loc[:,['cases','deaths']] = norm_df_bef_eu[['cases','deaths']]
+do_graph_clustering(covid_stats_bef_eu,"Cluster of countries before vaccination in European Union")
 
-#Cluster of countries according to cases and deaths after vaccinations
+#Cluster of countries according to cases, deaths, IFR, FirstDose, SecondDose, DoseAdditional1 and DoseAdditional1 after vaccinations in EU
 
-covid_stats_after_vacc = pd.read_csv("./data/eu_covid_19(2021-2022).csv")
-covid_stats_after_vacc.fillna(method="ffill", inplace=True)
-cols_after_vacc = ["FirstDose","SecondDose","DoseAdditional1","DoseAdditional2","cases","deaths"]
-norm_df_bef_vacc = (covid_stats_after_vacc[cols_after_vacc]-covid_stats_after_vacc[cols_after_vacc].mean())/(covid_stats_after_vacc[cols_after_vacc].max()-covid_stats_after_vacc[cols_after_vacc].min())
-covid_stats_after_vacc.loc[:,cols_after_vacc] = norm_df_bef_vacc[cols_after_vacc]
-do_graph_clustering(covid_stats_after_vacc,"Cluster of countries after vaccination")
+covid_stats_after_eu = pd.read_csv("./data/eu_covid_19(2021-2022).csv")
+cols_after_eu = ["FirstDose","SecondDose","DoseAdditional1","DoseAdditional2","cases","deaths","IFR"]
+norm_df_after_eu = (covid_stats_after_eu[cols_after_eu]-covid_stats_after_eu[cols_after_eu].mean())/(covid_stats_after_eu[cols_after_eu].max()-covid_stats_after_eu[cols_after_eu].min())
+covid_stats_after_eu.loc[:,cols_after_eu] = norm_df_after_eu[cols_after_eu]
+do_graph_clustering(covid_stats_after_eu,"Cluster of countries after vaccination in European Union")
+
+#Cluster of countries according to cases, deaths and IFR before vaccinations in Europe
+
+covid_stats_before_europe = pd.read_csv("./data/europe_continent_covid(2020).csv")
+norm_df_bef_europe = (covid_stats_before_europe[['cases','deaths']]-covid_stats_before_europe[['cases','deaths']].mean())/(covid_stats_before_europe[['cases','deaths']].max()-covid_stats_before_europe[['cases','deaths']].min())
+covid_stats_before_europe.loc[:,['cases','deaths']] = norm_df_bef_europe[['cases','deaths']]
+do_graph_clustering(covid_stats_before_europe,"Cluster of countries before vaccination in Europe")
+
+#Cluster of countries according to cases, deaths and IFR before vaccinations in Europe
+
+covid_stats_after_europe = pd.read_csv("./data/europe_continent_covid(2021-2022).csv")
+norm_df_after_europe = (covid_stats_after_europe[['cases','deaths']]-covid_stats_after_europe[['cases','deaths']].mean())/(covid_stats_after_europe[['cases','deaths']].max()-covid_stats_after_europe[['cases','deaths']].min())
+covid_stats_after_europe.loc[:,['cases','deaths']] = norm_df_after_europe[['cases','deaths']]
+do_graph_clustering(covid_stats_after_europe,"Cluster of countries after vaccination in Europe")
+
+#Cluster of countries according to cases, deaths and IFR before vaccinations in Asia
+
+covid_stats_before_asia = pd.read_csv("./data/asia_continent_covid(2020).csv")
+norm_df_bef_asia = (covid_stats_before_asia[['cases','deaths']]-covid_stats_before_asia[['cases','deaths']].mean())/(covid_stats_before_asia[['cases','deaths']].max()-covid_stats_before_asia[['cases','deaths']].min())
+covid_stats_before_asia.loc[:,['cases','deaths']] = norm_df_bef_asia[['cases','deaths']]
+do_graph_clustering(covid_stats_before_asia,"Cluster of countries before vaccination in Asia")
+
+#Cluster of countries according to cases, deaths and IFR before vaccinations in Asia
+
+covid_stats_after_asia = pd.read_csv("./data/asia_continent_covid(2021-2022).csv")
+norm_df_after_asia = (covid_stats_after_asia[['cases','deaths']]-covid_stats_after_asia[['cases','deaths']].mean())/(covid_stats_after_asia[['cases','deaths']].max()-covid_stats_after_asia[['cases','deaths']].min())
+covid_stats_after_asia.loc[:,['cases','deaths']] = norm_df_after_asia[['cases','deaths']]
+do_graph_clustering(covid_stats_after_asia,"Cluster of countries after vaccination in Asia")
+
+#Cluster of countries according to cases, deaths and IFR before vaccinations in Oceania
+
+covid_stats_before_oceania = pd.read_csv("./data/oceania_continent_covid(2020).csv")
+norm_df_bef_oceania = (covid_stats_before_oceania[['cases','deaths']]-covid_stats_before_oceania[['cases','deaths']].mean())/(covid_stats_before_oceania[['cases','deaths']].max()-covid_stats_before_oceania[['cases','deaths']].min())
+covid_stats_before_oceania.loc[:,['cases','deaths']] = norm_df_bef_oceania[['cases','deaths']]
+do_graph_clustering(covid_stats_before_oceania,"Cluster of countries before vaccination in Oceania")
+
+#Cluster of countries according to cases, deaths and IFR before vaccinations in Oceania
+
+covid_stats_after_oceania = pd.read_csv("./data/oceania_continent_covid(2021-2022).csv")
+norm_df_after_oceania = (covid_stats_after_oceania[['cases','deaths']]-covid_stats_after_oceania[['cases','deaths']].mean())/(covid_stats_after_oceania[['cases','deaths']].max()-covid_stats_after_oceania[['cases','deaths']].min())
+covid_stats_after_oceania.loc[:,['cases','deaths']] = norm_df_after_oceania[['cases','deaths']]
+do_graph_clustering(covid_stats_after_oceania,"Cluster of countries after vaccination in Oceania")
+
+#Cluster of countries according to cases, deaths and IFR before vaccinations in America
+covid_stats_before_america = pd.read_csv("./data/america_continent_covid(2020).csv")
+norm_df_bef_america = (covid_stats_before_america[['cases','deaths']]-covid_stats_before_america[['cases','deaths']].mean())/(covid_stats_before_america[['cases','deaths']].max()-covid_stats_before_america[['cases','deaths']].min())
+covid_stats_before_america.loc[:,['cases','deaths']] = norm_df_bef_america[['cases','deaths']]
+do_graph_clustering(covid_stats_before_america,"Cluster of countries before vaccination in America")
+
+#Cluster of countries according to cases, deaths and IFR after vaccinations in America
+covid_stats_after_america = pd.read_csv("./data/america_continent_covid(2021-2022).csv")
+norm_df_after_america = (covid_stats_after_america[['cases','deaths']]-covid_stats_after_america[['cases','deaths']].mean())/(covid_stats_after_america[['cases','deaths']].max()-covid_stats_after_america[['cases','deaths']].min())
+covid_stats_after_america.loc[:,['cases','deaths']] = norm_df_after_america[['cases','deaths']]
+do_graph_clustering(covid_stats_after_america,"Cluster of countries after vaccination in America")
+
+#Cluster of continents according to cases, deaths and IFR before vaccinations
+covid_stats_before_continents = pd.read_csv("./data/continents_covid(2020).csv")
+norm_df_bef_continents = (covid_stats_before_continents[['cases','deaths']]-covid_stats_before_continents[['cases','deaths']].mean())/(covid_stats_before_continents[['cases','deaths']].max()-covid_stats_before_continents[['cases','deaths']].min())
+covid_stats_before_continents.loc[:,['cases','deaths']] = norm_df_bef_continents[['cases','deaths']]
+do_graph_clustering(covid_stats_before_continents,"Cluster of continents before vaccination")
+
+#Cluster of contitents according to cases, deaths and IFR after vaccinations
+covid_stats_after_continents = pd.read_csv("./data/continents_covid(2021-2022).csv")
+norm_df_after_continents = (covid_stats_after_continents[['cases','deaths']]-covid_stats_after_continents[['cases','deaths']].mean())/(covid_stats_after_continents[['cases','deaths']].max()-covid_stats_after_continents[['cases','deaths']].min())
+covid_stats_after_continents.loc[:,['cases','deaths']] = norm_df_after_continents[['cases','deaths']]
+do_graph_clustering(covid_stats_after_continents,"Cluster of continents after vaccination")
